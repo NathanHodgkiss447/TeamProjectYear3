69c8b1998237990b818e883937531053b2f66ec8
Alex-Plktt <alex.plktt432@gmail.com>
2019-10-17 13:36:05 +0100

    login page

---
+++ app/src/main/java/com/example/teamprojectyear3/LoginActivity.java
@@ -0,0 +1,18 @@
+package com.example.teamprojectyear3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override void onCreate(Bundle savedInstantStance) {
+        super.onCreate(savedInstantStance);
+        setContentView(R.layout.activity_login);
+    }
+
+    //defining views
+    private Button ButtonSignIn;
+
+}
