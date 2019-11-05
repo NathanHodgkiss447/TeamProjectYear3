8aab94c07817f1f274880ae853caf6e4ccb9123e
Alex-Plktt <alex.plktt432@gmail.com>
2019-11-05 00:25:07 +0000

    Added user login system and created registration activity for user registration system

--- app/src/main/java/com/example/teamprojectyear3/LoginActivity.java
+++ app/src/main/java/com/example/teamprojectyear3/LoginActivity.java
@@ -1,7 +1,11 @@
 package com.example.teamprojectyear3;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -9,14 +13,7 @@ import android.widget.TextView;
 import com.google.firebase.auth.FirebaseAuth;
 
 
-public class LoginActivity extends AppCompatActivity {
-
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-    }
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
     //defining views
     private Button buttonSignIn;
@@ -25,9 +22,64 @@ public class LoginActivity extends AppCompatActivity {
     private TextView textViewUserSignIn;
 
     //DECLARE FIREBASE AUTH
-    private FirebaseAuth FirebaseAuth;
+    private FirebaseAuth firebaseAuth;
 
     //declare progress dialog
+    private ProgressDialog progressDialog;
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
 
+        //initialize firebase
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        if (firebaseAuth.getInstance() != null) {
+            startActivity(new Intent(this, HomeActivity.class));
+        }
+
+        //initialize views
+        editTextEmail = (EditText) findViewById(R.id.editTextEmail);
+        editTextPassword = (EditText) findViewById(R.id.editTextPassword);
+        buttonSignIn = (Button) findViewById(R.id.buttonSignIn);
+        textViewUserSignIn = (TextView) findViewById(R.id.textViewUserSignIn);
+
+        progressDialog = new ProgressDialog(this);
+
+        buttonSignIn.setOnClickListener(this);
+        textViewUserSignIn.setOnClickListener(this);
+    }
+
+    private void userLogin() {
+        String email = editTextEmail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+
+        if (email.isEmpty()) {
+            editTextEmail.setError("Email is required");
+            editTextEmail.requestFocus();
+            return;
+        }
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            editTextEmail.setError("Please Enter a valid email address");
+            editTextEmail.requestFocus();
+            return;
+        }
+        if (password.isEmpty()) {
+            editTextPassword.setError("Password is required");
+            editTextPassword.requestFocus();
+            return;
+        }
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == buttonSignIn) {
+            userLogin();
+        }
+        if (view == textViewUserSignIn) {
+            startActivity(new Intent(this, RegistrationActivity.class));
+        }
+    }
 }
