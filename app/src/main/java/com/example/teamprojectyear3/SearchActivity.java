0250d477a48cd665f3600203efbe1cac754c9588
Dylan <dyevki@gmail.com>
2019-11-19 14:12:09 +0000

    JSON

--- app/src/main/java/com/example/teamprojectyear3/SearchActivity.java
+++ app/src/main/java/com/example/teamprojectyear3/SearchActivity.java
@@ -7,10 +7,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
 
 public class SearchActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Button buttonella;
+    public String Grad;
+    public String Grad2;
     public void onClick (View view) {
         //if logout is pressed
         if (view == buttonella) {
@@ -30,62 +35,85 @@ public class SearchActivity extends AppCompatActivity implements View.OnClickLis
 
 
 
-            //Initialising views
-            buttonella = (Button) findViewById(R.id.buttonella);
-            ImageView imgF = (ImageView) findViewById(R.id.facebook);
-            ImageView imgT = (ImageView) findViewById(R.id.twitter);
-            ImageView imgI = (ImageView) findViewById(R.id.instagram);
-            ImageView imgL = (ImageView) findViewById(R.id.linkedin);
+        //Initialising views
+        buttonella = (Button) findViewById(R.id.buttonella);
+        ImageView imgF = (ImageView) findViewById(R.id.facebook);
+        ImageView imgT = (ImageView) findViewById(R.id.twitter);
+        ImageView imgI = (ImageView) findViewById(R.id.instagram);
+        ImageView imgL = (ImageView) findViewById(R.id.linkedin);
 
-            buttonella.setOnClickListener(this);
+        buttonella.setOnClickListener(this);
 
-            //Facebook launching on icon tap
-            imgF.setOnClickListener(new View.OnClickListener() {
-                public void onClick(View v) {
-                    Intent intent = new Intent();
-                    intent.setAction(Intent.ACTION_VIEW);
-                    intent.addCategory(Intent.CATEGORY_BROWSABLE);
-                    intent.setData(Uri.parse("http://facebook.com"));
-                    startActivity(intent);
-                }
-            });
-
-            //Twitter launching on icon tap
-            imgT.setOnClickListener(new View.OnClickListener() {
-                public void onClick(View v) {
-                    Intent intent = new Intent();
-                    intent.setAction(Intent.ACTION_VIEW);
-                    intent.addCategory(Intent.CATEGORY_BROWSABLE);
-                    intent.setData(Uri.parse("http://twitter.com"));
-                    startActivity(intent);
-                }
-            });
+        //get the spinner from the xml.
+        Spinner dropdown = findViewById(R.id.spinner1);
+//create a list of items for the spinner.
+        String[] items = new String[]{"Graduate Qualification", "College"};
+//create an adapter to describe how the items are displayed, adapters are used in several places in android.
+//There are multiple variations of this, but this is the basic variant.
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
+//set the spinners adapter to the previously created one.
+        dropdown.setAdapter(adapter);
 
 
-            //Linkedin launching on icon tap
-            imgL.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Intent intent = new Intent();
-                    intent.setAction(Intent.ACTION_VIEW);
-                    intent.addCategory(Intent.CATEGORY_BROWSABLE);
-                    intent.setData(Uri.parse("https://www.instagram.com/?hl=en"));
-                    startActivity(intent);
-                }
-            });
 
-            //Instagram launching on icon tap
-            imgI.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Intent intent = new Intent();
-                    intent.setAction(Intent.ACTION_VIEW);
-                    intent.addCategory(Intent.CATEGORY_BROWSABLE);
-                    intent.setData(Uri.parse("https://www.instagram.com/?hl=en"));
-                    startActivity(intent);
-                }
-            });
 
-        }
+
+
+        PHPActivity p =new PHPActivity();
+
+
+        p.setGrad(Grad);
+        p.setGrad2(Grad2);
+
+        //Facebook launching on icon tap
+        imgF.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                intent.setData(Uri.parse("http://facebook.com"));
+                startActivity(intent);
+            }
+        });
+
+        //Twitter launching on icon tap
+        imgT.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                intent.setData(Uri.parse("http://twitter.com"));
+                startActivity(intent);
+            }
+        });
+
+
+        //Linkedin launching on icon tap
+        imgL.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                intent.setData(Uri.parse("https://www.instagram.com/?hl=en"));
+                startActivity(intent);
+            }
+        });
+
+        //Instagram launching on icon tap
+        imgI.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                intent.setData(Uri.parse("https://www.instagram.com/?hl=en"));
+                startActivity(intent);
+            }
+        });
 
     }
+
+
+
+}
