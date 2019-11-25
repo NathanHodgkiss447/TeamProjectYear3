90938db3274bcb7a7164d5e41e0e097ad4016ea1
Piyush Sharma <piyush.sharma1078@gmail.com>
2019-11-25 14:05:45 +0000

    firebase

--- app/src/main/java/com/example/teamprojectyear3/SettingActivity.java
+++ app/src/main/java/com/example/teamprojectyear3/SettingActivity.java
@@ -2,30 +2,63 @@ package com.example.teamprojectyear3;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
 public class SettingActivity extends AppCompatActivity {
+
+    private Button buttonSubmit;
+    private EditText editName;
+    private EditText editCompany;
+    private EditText editTitle;
+    private EditText editEmail;
+
+
+    private static final String TAG = "SettingActivity";
+
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
 
-
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
 
 
         //Initialising views
 
-        ImageView imgF = (ImageView)findViewById(R.id.facebook);
-        ImageView imgT = (ImageView)findViewById(R.id.twitter);
-        ImageView imgI = (ImageView)findViewById(R.id.instagram);
-        ImageView imgL = (ImageView)findViewById(R.id.linkedin);
+        ImageView imgF = (ImageView) findViewById(R.id.facebook);
+        ImageView imgT = (ImageView) findViewById(R.id.twitter);
+        ImageView imgI = (ImageView) findViewById(R.id.instagram);
+        ImageView imgL = (ImageView) findViewById(R.id.linkedin);
+        editName = (EditText) findViewById(R.id.editTextName);
+        editCompany = (EditText) findViewById(R.id.editCompany);
+        editTitle = (EditText) findViewById(R.id.editTitle);
+        editEmail = (EditText) findViewById(R.id.editEmail);
+        buttonSubmit = (Button) findViewById(R.id.buttonSubmit);
+
+
+
 
 
         //Facebook launching on icon tap
-        imgF.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
+        imgF.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
                 Intent intent = new Intent();
                 intent.setAction(Intent.ACTION_VIEW);
                 intent.addCategory(Intent.CATEGORY_BROWSABLE);
@@ -35,8 +68,8 @@ public class SettingActivity extends AppCompatActivity {
         });
 
         //Twitter launching on icon tap
-        imgT.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
+        imgT.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
                 Intent intent = new Intent();
                 intent.setAction(Intent.ACTION_VIEW);
                 intent.addCategory(Intent.CATEGORY_BROWSABLE);
@@ -72,4 +105,39 @@ public class SettingActivity extends AppCompatActivity {
 
     }
 
+    private void SubmitUser(){
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+// Create a new user with a first, middle, and last name
+        Map<String, Object> Employer = new HashMap<>();
+        Employer.put("Name", editName);
+        Employer.put("Company", editCompany);
+        Employer.put("Title", editTitle);
+        Employer.put("Email", editEmail);
+
+
+// Add a new document with a generated ID
+        db.collection("Employer" + editEmail)
+                .add(Employer)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d(TAG,"DocumentSnapshot added with ID: " + documentReference.getId());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error adding document", e);
+                    }
+                });
+    }
+
+    public void submit(View view) {
+
+        if (view == buttonSubmit) {
+            SubmitUser();
+        }
+    }
 }
