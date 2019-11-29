23ca1a409b55721f4333bfef1832b40b6029d588
Dylan <dyevki@gmail.com>
2019-11-29 13:19:09 +0000

    JSON

--- app/src/main/java/com/example/teamprojectyear3/PHPActivity.java
+++ app/src/main/java/com/example/teamprojectyear3/PHPActivity.java
@@ -12,26 +12,41 @@ import android.widget.Toast;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.Toast;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class PHPActivity extends AppCompatActivity {
 
-    ListView listView;
-    public String Grad;
+   // String textView;
+   // public String Grad;
+    //String textView2;
 
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activityphp);
+        setContentView(R.layout.adapter_view_layout);
 
 
-        listView = findViewById(R.id.listView);
+        //textView = findViewById(R.id.textView);
+        //textView2 = findViewById(R.id.textView2);
 
             getJSON("http://phpmyadminnci.gearhostpreview.com/jsongrad.php");
 
@@ -56,7 +71,7 @@ public class PHPActivity extends AppCompatActivity {
             @Override
             protected void onPostExecute(String s) {
                 super.onPostExecute(s);
-               // Toast.makeText(getApplicationContext(), s, Toast.LENGTH_SHORT).show();
+               //Toast.makeText(getApplicationContext(), s, Toast.LENGTH_SHORT).show();
                 try {
                     loadIntoListView(s);
                 } catch (JSONException e) {
@@ -90,11 +105,36 @@ public class PHPActivity extends AppCompatActivity {
         String[] dylan = new String[jsonArray.length()];
         for (int i = 0; i < jsonArray.length(); i++) {
             JSONObject obj = jsonArray.getJSONObject(i);
-            dylan[i] = obj.getString("FPCHS" + "TM");
+            String id= obj.getString("FCPHS");
+
+
+           String Variable = obj.getString("Male Certificates");
+
+            HashMap<String, String> contact = new HashMap<>();
+
+
+
+            contact.put("ID", id);
+            contact.put("Variable", Variable);
+
+
 
         }
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, dylan);
-        listView.setAdapter(arrayAdapter);
+
+      /*  for (int i = 0; i < jsonArray.length(); i++) {
+            JSONObject obj = jsonArray.getJSONObject(i);
+
+            Toast.makeText(getApplicationContext(), i, Toast.LENGTH_SHORT).show();
+            ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, dylan);
+*  @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+            ListAdapter adapter = new SimpleAdapter(MainActivity.this, contactList,
+                    R.layout.list_item, new String[]{ "email","mobile"},
+                    new int[]{R.id.id, R.id.VARIABLE});
+            lv.setAdapter(adapter);
+        }*/
+
     }
 
 }
